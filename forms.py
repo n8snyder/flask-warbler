ac8cafd2753b1dd5d6c71951a69353b8c6228d79
Nate Snyder <n8.snyder@berkeley.edu>
2022-02-09 12:08:07 -0800

    Add route and form for edit the logged in user

--- forms.py
+++ forms.py
@@ -18,6 +18,17 @@ class UserAddForm(FlaskForm):
     image_url = StringField("(Optional) Image URL")
 
 
+class UserEditForm(FlaskForm):
+    """Form for editing users."""
+
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    image_url = StringField("Image URL")
+    header_image_url = StringField(("Header Image URL"))
+    bio = StringField("Bio")
+    password = PasswordField("Password", validators=[DataRequired()])
+
+
 class LoginForm(FlaskForm):
     """Login form."""
 
